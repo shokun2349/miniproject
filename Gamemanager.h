f1d910da0eba15311ce6b72b6f966d88b1f9aaf3
shokun2349 <shokun2349@gmail.com>
2026-02-27 02:08:45 +0700

    main menu

--- Gamemanager.h
+++ Gamemanager.h
@@ -4,7 +4,7 @@
 #include "BattleSystem.h"
 
 // สถานะของเกมในปัจจุบัน
-enum GameState { PLAYING, STAGE_CLEAR, GAME_OVER, GAME_WON };
+enum GameState { MAIN_MENU, PLAYING, ANSWERING_MATH, STAGE_CLEAR, GAME_OVER, GAME_WON };
 
 class GameManager {
 public:
@@ -13,9 +13,19 @@ public:
     int currentStage;
     GameState state;
 
+    // ตัวแปรสำหรับระบบคณิตศาสตร์
+    std::string mathQuestion;
+    int mathAnswer;
+    std::string playerInputString;
+    bool isQueuedUlt; // จำไว้ว่าผู้เล่นกดโจมตีปกติ หรือ ท่าไม้ตาย
+
     GameManager();
     void initPlayerTeam();
     void startStage(int stage);
     void updateBattleState(); // เช็คผลแพ้ชนะหลังจบเทิร์น
     void proceedToNextStage();
+
+    // ฟังก์ชันใหม่สำหรับคณิตศาสตร์
+    void triggerMathQuestion(bool isUlt);
+    void submitMathAnswer();
 };
